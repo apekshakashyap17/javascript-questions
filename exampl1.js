9394e57ac53e2811d7c1653e0930b88e025e2c20
apekshakashyap17 <apeksha.kashyap17@gmail.com>
2024-11-25 17:17:25 +0530

    added js 1

---
+++ exampl1.js
@@ -0,0 +1,11 @@
+//day 1 practice question1
+
+const object1 = {
+    name: "parker jotter standard ball pen",
+    rating: 4,
+    MRP: 285,
+    price: 270,
+    discount: "5%"
+}
+
+console.log(object1);
