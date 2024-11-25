c09c4863db4dda2a99d112ab46800f179c2df7e9
apekshakashyap17 <apeksha.kashyap17@gmail.com>
2024-11-25 17:20:09 +0530

    added js 2

---
+++ example2.js
@@ -0,0 +1,12 @@
+//DAY 1 practice question2 
+
+const instapfp = {
+    name: "poosh",
+    posts: 573,
+    followers: "3.1M",
+    following: 81,
+    bio: "Brand by @kourtneykardash a guide to living your best life."
+
+};
+
+console.log(instapfp);
